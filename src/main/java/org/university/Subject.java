cda20ca84aede55883100a298bccf2e4bbef726c
charles-holguin_globant <charles.holguin@globant.com>
2024-12-02 22:19:59 -0500

    Implement HashMap to save information Teacher and Student

--- src/main/java/org/university/Subject.java
+++ src/main/java/org/university/Subject.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Subject {
     private String nameSubject;
     private ArrayList<Student> storeStudentsBySubject;
+    private Teacher teacher;
 
 
     public Subject(String nameSubject){
@@ -12,14 +13,30 @@ public class Subject {
         this.storeStudentsBySubject = new ArrayList<>();
     }
 
-    public void addStudent(Student student){
-        storeStudentsBySubject.add(student);
-        System.out.println(student.getName() + "Has been added " + nameSubject);
+    public void assignTeacherToSubject(Teacher teacher){
+        this.teacher = teacher;
+        System.out.println(teacher.getName() + " has been assigned to " + nameSubject);
     }
 
-    public void displayStudents(){
-        for (Student student : storeStudentsBySubject){
+
+    public void addStudent(Student student){
+        storeStudentsBySubject.add(student);
+        System.out.println(student.getName() + " Has been added " + nameSubject);
+    }
+
+    public void displayStudents() {
+        for (Student student : storeStudentsBySubject) {
             student.displayInfoPerson();
+        }
+    }
+
+    public void displaySubjectDetails() {
+        System.out.println("\nSubject: " + nameSubject);
+        if (teacher != null) {
+            System.out.println("Teacher:");
+            teacher.displayInfoPerson();
+        } else {
+            System.out.println("No teacher assigned yet.");
         }
     }
 }
--- src/main/java/org/university/University.java
+++ src/main/java/org/university/University.java
@@ -1,11 +1,46 @@
 package org.university;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class University {
     public static void main(String[] args) {
+        ArrayList<Integer> idsStudents;
 
+        //Database HashMap Teacher
+        HashMap<Integer, Teacher> teacherDatabase = new HashMap<>();
+        teacherDatabase.put(10, new Teacher(1, "Teacher1", "Bruno", 40000,100));
+        teacherDatabase.put(20, new Teacher(2,"Teacher2", "Rios", 3000, 101));
+
+        //Database HashMap Students
+        HashMap<Integer, Student> studentDatabase = new HashMap<>();
+        studentDatabase.put(1, new Student(1, "Charles", "Holguin", 41));
+        studentDatabase.put(2, new Student(2,"Liz", "Gonzalez", 35));
+
+        //Crear Subject
         Subject subjectJava = new Subject("Java");
 
-        System.out.println("");
+        // Assign a teacher by ID
+        Integer teacherID = 10;
+        if (teacherDatabase.containsKey(teacherID)) {
+            subjectJava.assignTeacherToSubject((teacherDatabase.get(teacherID)));
+        } else {
+            System.out.println("Teacher with ID " + teacherID + " not found.");
+        }
+
+        // Enroll students by ID
+        //int[] studentIDs = {1,2};
+        idsStudents = new ArrayList<>();
+        idsStudents.add(1);
+        for (int studentID : idsStudents) {
+            if (studentDatabase.containsKey(studentID)) {
+                subjectJava.addStudent((studentDatabase.get(studentID)));
+            } else {
+                System.out.println("Student with ID " + studentID + " not found.");
+            }
+        }
+
+
+        System.out.println("Mostrar data profesores");
         Teacher teacher1 = new Teacher(11,"Felipe","Gomez",4000,1);
         teacher1.displayInfoPerson();
 
@@ -19,13 +54,18 @@ public class University {
         subjectJava.addStudent(studen2);
 
         System.out.println("");
+        System.out.println("displayStudents: ");
         subjectJava.displayStudents();
 
         System.out.println("");
         Student newStudent = new Student(777,"Test","CH",18);
         subjectJava.addStudent(newStudent);
-        System.out.println("");
+        System.out.println("new student ch");
         subjectJava.displayStudents();
+
+        // Display subject details
+        System.out.println("Detalles de la subject ");
+        subjectJava.displaySubjectDetails();
 
 
     }
